54dfbac99580f59fb364cc4af52c545ba63a07f0
maxjeffwell <maxjeffwell@gmail.com>
2018-07-17 19:43:57 -0400

    fixed syntax error

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ function max(numbers) {
         }
         i++;
     }
+  }
 
   //_________________________________________________________________
 
@@ -64,7 +65,24 @@ function filter(arr, fn) {
   }
   return newArray;
 }
-console.log(filter(names, name => name[0] === 'R'));
+
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+const myNames = [
+  'Rich', 'Joe', 'Bhaumik', 'Ray'
+];
+
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, function(name) {
+    // This is known as a "predicate function" - it's a function that 
+    // only returns a boolean
+    return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
+
 
 //_____________________________________________________________
 
@@ -119,7 +137,7 @@ const message = input.split(' ').reduce((resultString, word) => {
        else {
            return resultString + word[word.length - 1].toUpperCase();
        }
-   }, '');
+},'');
 console.log(message);
 
 
