05b638fe92944ecceb392bcb44bff7c1bdcfed22
maxjeffwell <maxjeffwell@gmail.com>
2018-07-17 14:14:55 -0400

    started higher order drills

--- main.js
+++ main.js
@@ -11,13 +11,42 @@ function max(numbers) {
     return largestNumber;
   }
 
-let sum = 0;
+
 
 function computeAverageOfNumbers(numbers) {
+  let sum = 0;
   numbers.forEach(function(numbers) {
   sum += numbers;
   });
   return sum/numbers.length;
 }
   
+function repeat(fn, n) {
+  for (let i = 0; i < n; i++) {
+    fn();
+  }
+};
 
+
+function hello() {
+  console.log("Hello " + "world"); 
+}
+
+function goodbye() {
+  console.log("Goodbye " + "world");
+}
+
+repeat(hello, 5);
+repeat(goodbye, 5);
+
+
+
+function filter(arr, fn) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i]) === 'true') {
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
+}
